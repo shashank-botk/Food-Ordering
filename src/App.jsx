b179c6d64ca3685e4599db192ce24d2b12c14a3d
Satyajeet-23 <satyajeett023@outlook.com>
2025-06-14 13:46:19 +0530

    Intialize Tailwind CSS Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Food Ordering App</h1>
             <h1>Food</h1>
       <h1>Ordering</h1>
-      <h1>App</h1>
+      <h1 className="txt" >App</h1>
     </>
   )
 }
